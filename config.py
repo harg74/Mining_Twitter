b577b9d3d66fd78fa8ea865d2eaed983e5de36c1
Humberto Rodriguez <78564912+harg74@users.noreply.github.com>
2022-01-10 12:25:18 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-access_token='266910255-w42JP8MnCPOUtorQnzyVf4Hn0lSPIbJLRpWGqdAO'
-
-access_token_secret='6a9omYn8EfcDB9XX5nHEvGGD18CrUTsEWcJVeScDrO1Uj'
-
-api_key='btUygxlb0zi7EBNpzpFs43E4D'
-
-api_secret='4B7KMCx73cXKuKJAhHPstR8NlofammlLw4oGQdvyLaRIG8dZVf'
